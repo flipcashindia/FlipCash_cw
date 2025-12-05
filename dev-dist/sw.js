5cbe14fc9175fc2cfe1096de60c57d11a6c23195
nae-imUam <nae.imuam103@gmail.com>
2025-12-05 08:07:13 +0530

    update visit flow

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-e755d862'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.a488a712nj"
+    "revision": "0.lqvldt0no2k"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
