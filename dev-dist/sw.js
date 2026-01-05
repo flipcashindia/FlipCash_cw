9345147069bc92cb88b24ebee47d048617836b0d
nae-imUam <nae.imuam103@gmail.com>
2026-01-05 12:31:11 +0530

    make change in the price estimation

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-e755d862'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.e28t1q3lfi"
+    "revision": "0.ubg1dg6ivrs"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
